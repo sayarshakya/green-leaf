a564c25a2eb2fb08948952746e7082f659cfdbc9
Sayar Shakya <sakyashire@hotmail.com>
2025-07-10 09:22:47 +0545

    layout change for login

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,8 @@
 import '@/app/globals.css';
 
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return(
-         <html>
+         <html lang="en">
              <body className="h-screen m-0 p-0 overflow-hidden flex items-center justify-center bg-gray-100">
                  <main className="flex-1 pt-16 pb-16 p-6 bg-white">
                      {children}
