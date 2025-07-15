f01fae451c0d926350504c0c34e02a0975191af3
Sayar Shakya <sakyashire@hotmail.com>
2025-07-15 17:30:20 +0545

    email sender added

---
+++ src/app/api/send-email/route.ts
@@ -0,0 +1,55 @@
+// pages/api/send-email.ts
+import nodemailer from 'nodemailer';
+import { NextResponse } from 'next/server';
+import { db } from '@/lib/firebaseAdmin';
+
+export const dynamic = "force-dynamic";
+
+export async function GET() {
+
+  const subject = 'Monthly Deposit Reminder from Green Leaf';
+  const text = `Hello!
+
+    This is your monthly update from Green Leaf.
+
+    Stay green and keep growing!,
+    Team Green Leaf`;
+
+  try {
+    //const snapshot = await db.collection('users').get();
+    //const toList: string[] = [];
+    const toList: string[] = [
+      "sayarshakya@gmail.com",
+    ];
+
+    // snapshot.forEach((doc) => {
+    //   const data = doc.data();
+    //   if (data.email) {
+    //     toList.push(data.email);
+    //   }
+    // });
+
+    if (toList.length === 0) {
+      return NextResponse.json({ message: 'No user emails found.' }, { status: 400 });
+    }
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    await transporter.sendMail({
+      from: `"Green Leaf" <${process.env.EMAIL_USER}>`,
+      bcc: toList.join(','),
+      subject,
+      text,
+    });
+   return NextResponse.json({ message: `Email sent to ${toList.length} users.` });
+  } catch (error) {
+    console.error('Email sending failed:', error);
+    return NextResponse.json({ message: 'Email sending failed.', error }, { status: 500 });
+  }
+}
